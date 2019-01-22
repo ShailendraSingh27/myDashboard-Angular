469937f5b565b30ced17d2eb7a3d8af0bdc59118
Shailendra Singh <ShailendraSingh27@users.noreply.github.com>
2019-01-22 23:53:27 +0530

    added tabs to education

--- src/app/profile-info/profile-info.component.ts
+++ src/app/profile-info/profile-info.component.ts
@@ -5,11 +5,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './profile-info.component.html',
   styleUrls: ['./profile-info.component.css']
 })
-export class ProfileInfoComponent implements OnInit {
+export class ProfileInfoComponent {
 
   constructor() { }
 
-  ngOnInit() {
+  isMCAActive=false;
+  isBCAActive=true;
+  isXIIActive=false;
+
+  onClickMCA(){
+    this.isMCAActive=true;
+    this.isBCAActive=false;
+    this.isXIIActive=false;
   }
+
+  onClickBCA(){
+    this.isMCAActive=false;
+    this.isBCAActive=true;
+    this.isXIIActive=false;
+  }
+
+  onClickXII(){
+    this.isMCAActive=false;
+    this.isBCAActive=false;
+    this.isXIIActive=true;
+  }
+
 
 }
